51cfeee29944a03a0e77527754b6c221833d98e5
Thomas Muir <ThomasMuIT@proton.me>
2025-02-14 12:59:39 -0800

    Updated scripts to have absolute links to elements

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    fetch("footer.html")
+    fetch("/footer.html")
         .then(response => {
             if (!response.ok) {
                 throw new Error(`Footer fetch failed: ${response.status}`);
